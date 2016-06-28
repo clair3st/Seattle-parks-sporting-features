89902a5305d881aa4dd0bfbcef7c611969b45f28
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-28 14:40:11 -0700

    closes #18 function to handle click on marker

--- scripts/views/parkInfoView.js
+++ scripts/views/parkInfoView.js
@@ -1,22 +1,36 @@
-// (function(module){
-//
-//   marker.addListener('click', function() {
-//     $('.tab-content').hide();
-//     $('#park-info').append(parkView.toHtml(this, '#park-template'));
-//     $('#park-info').show();
-//   });
-//
-//   module.parkInfoView = parkInfoView;
-// })(window);
-ParkData.getAllSportsArray();
-var test = function(){
-  ParkData.allSportsArray.forEach(function(a) {
-    var marker = new google.maps.Marker({
-      position: {lat: a.lng, lng: a.lat},
-      map: map,
-      title: a.name
-    });
+(function (module){
 
-    console.log(a.name);
-  });
-};
+  var parkInfoView = {};
+
+  parkInfoView.handleBack = function() {
+    $('#back-link').on('click', function(){
+      $('.tab-content').hide();
+      $('#map').show();
+      $('#user-form-container').show();
+    });
+  };
+
+
+  ParkData.getAllSportsArray();
+
+
+  parkInfoView.addMarkers = function() {
+    var marker;
+    ParkData.allSportsArray.forEach(function(a) {
+      marker = new google.maps.Marker({
+        position: {lat: a.lng, lng: a.lat},
+        map: map
+      });
+      marker.addListener('click', function() {
+        $('.tab-content').hide();
+        $('#park-info').empty();
+        $('#park-info').append(parkView.toHtml(a, '#park-template'));
+        $('#park-info').show();
+        parkInfoView.handleBack();
+      });
+    });
+  };
+
+  module.parkInfoView = parkInfoView;
+
+})(window);
