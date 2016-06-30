25d25af1030c93003dda9c6538ce9478d7f489c6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-30 14:36:00 -0700

    fixed handlebar bug

--- scripts/controllers/parksController.js
+++ scripts/controllers/parksController.js
@@ -2,11 +2,13 @@
   var parksController = {};
 
   parksController.index = function(ctx, next) {
+    console.log(ctx.parks);
     parksController.showInfoPage(ctx.parks);
   };
 
   parksController.showInfoPage = function(parks) {
     $('.tab-content').hide();
+    console.log(parks[0], 'park');
     $('#park-info').html(parkView.toHtml(parks[0], '#park-template'));
     $('#park-info').show();
     $('#feature-count').html('The number of facilities for this sport at this location is ' + parkView.selectedMarker.addressCount);
@@ -16,9 +18,10 @@
   parksController.loadById = function(ctx, next) {
     var parkCtxData = function(park){
       ctx.parks = park;
+      console.log(park);
       next();
     };
-
+    console.log(ctx.params.id);
     ParkData.findWhere('id', ctx.params.id, parkCtxData);
   };
 
